00d5f145e143b261b3fc659236400c423736a513
Mohamed Gamal <mohamedgamalfathy@gmail.com>
2022-08-26 10:46:49 +0200

    Upload script for sudo and run commands

---
+++ supersudo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script run sudo commands on remote hosts with password authentication 
+# password is in cleartext format
+# CopyRight by MohamedGamal (github.com/mogamal1)
+echo "This Script works only in **LINUX** MACHINES"
+if [ -z $1 ] 
+then 
+echo "Please run with argument $0 servers_list_file" ; exit 
+fi	
+
+for i in `cat $1`
+do
+echo "### $i ###"
+ssh "$i" 'echo PASSWORD_HERE_PLESE |sudo -S sh -c "sed -i '"'"'/^AllowUser/ s/$/ aabdel/'"'"' /etc/ssh/sshd_config;grep -c aabdel /etc/ssh/sshd_config;service sshd restart;usermod -aG wheel aabdel;id aabdel"'
+echo "### Done ###"
+done
