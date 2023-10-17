10f17e8d1dbe7eb7aca98ad76c6a6155d993e002
Mohamed Gamal <mohamedgamalfathy@gmail.com>
2023-10-17 12:16:27 +0300

    update file format

--- cis_final.sh
+++ cis_final.sh
@@ -6,7 +6,7 @@ then
 echo "[ERROR] Please run $0 $1 with root account"
 exit 10
 fi
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> Preparing /etc/fstab file"
 echo "Your fstab file is $WDIR/fstab"
 FSTAB=$WDIR/fstab
@@ -16,7 +16,7 @@ if [ `grep -w "/var/tmp" $FSTAB|grep -c defaults,` -eq 0 ]
 then
 sed -i  '/\/var\/tmp/s/defaults/&,nosuid,nodev,noexec/' $FSTAB
 fi
-#--------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------
 if [ `grep -v "^#" $FSTAB|grep -wc "/tmp"` -eq 0 ]
 then
 echo "tmpfs   /tmp   tmpfs  defaults,rw,nosuid,nodev,noexec   0 0" >> $FSTAB
@@ -24,12 +24,12 @@ elif [ `grep -w "/tmp" $FSTAB|grep -c defaults,` -eq 0 ]
 then
 sed -i  '/ \/tmp/s/defaults/&,nosuid,noexec,nodev/' $FSTAB
 fi
-#--------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------
 if [ `grep -w "/home" $FSTAB|grep -c defaults,` -eq 0 ]
 then
 sed -i  '/\/home/s/defaults/&,nodev/' $FSTAB
 fi
-#--------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------
 if [ `grep -v "^#" $FSTAB|grep -wc "/dev/shm"` -eq 0 ]
 then
 echo "tmpfs   /dev/shm   tmpfs  defaults,rw,nosuid,nodev,noexec  0 0" >> $FSTAB
@@ -37,14 +37,14 @@ elif [ `grep -w "/dev/shm" $FSTAB|grep -c nodev` -eq 0 ]
 then
 sed -i '/\/dev\/shm/s/\(\S\+\s\+\)\(\S\+\s\+\)\(\S\+\s\+\S\+\)\(\s\+\S\+\)/\1\2\3,nosuid,nodev,noexec\4/' $FSTAB
 fi
-#--------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------
 echo "Remounting all paritions. . ."
 mount -o remount /dev/shm
 mount -o remount /tmp
 mount -o remount /home
 mount -o remount /var/tmp
 echo "DONE"
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> Preparing /etc/modprobe.d/CIS.conf file"
 cat << EOF > /etc/modprobe.d/CIS.conf
 install cramfs /bin/true
@@ -62,13 +62,13 @@ install dccp /bin/true
 EOF
 
 echo "DONE"
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> Preparing Banner files"
 echo 'Authorized uses only. All activity may be monitored and reported.' > /etc/issue.net
 echo 'Authorized uses only. All activity may be monitored and reported.' > /etc/issue
 rm -f /etc/motd
 echo "DONE"
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> Preparing sysctl configs"
 cat << EOF > /etc/sysctl.d/CIS.conf
 net.ipv4.conf.all.accept_redirects=0
@@ -85,7 +85,6 @@ EOF
 echo "Applying sysctl configs. . . "
 sysctl -w net.ipv6.conf.all.accept_ra=0
 sysctl -w net.ipv6.conf.default.accept_ra=0
-sysctl -w net.ipv6.route.flush=1
 sysctl -w net.ipv6.conf.all.accept_redirects=0
 sysctl -w net.ipv6.conf.default.accept_redirects=0
 sysctl -w net.ipv6.route.flush=1
@@ -95,7 +94,7 @@ sysctl -w net.ipv4.conf.default.accept_redirects=0
 sysctl -w net.ipv4.conf.default.send_redirects=0
 sysctl -w net.ipv4.route.flush=1
 echo "DONE"
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> Preparing AIDE Setup"
 (dnf list installed aide || dnf -y install aide) \
 && test -f /var/lib/aide/aide.db.gz \
@@ -108,23 +107,23 @@ systemctl daemon-reload
 || echo "0 5 * * * /usr/sbin/aide --check" >> /var/spool/cron/root
 echo "AIDE service is scheduled and enabled."
 echo "DONE"
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> 1.1.21 Ensure sticky bit is set on all world-writable directories"
 #df --local -P|awk '{if (NR!=1) print $6}'|xargs -I '{}' find '{}' \
 #-xdev -type d ( -perm -0002 -a ! -perm -1000 ) 2>/dev/null|xargs -I '{}' chmod a+t '{}'
 echo "SKIPPED"
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> 4.2.3 Ensure permissions on all logfiles are configured"
 #find /var/log/ -type f -perm /g+wx,o+rwx -exec chmod g-wx,o-rwx '{}' +
 echo "SKIPPED"
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> 2.2.1.2 Ensure chrony is configured - user"
 grep -qE "^(server|pool)" /etc/chrony.conf || echo "[ERROR] Please configure NTP servers Pool"
 grep -qxF "OPTIONS=\"-u chrony\"" /etc/sysconfig/chronyd \
 || ( echo "OPTIONS=\"-u chrony\"" > /etc/sysconfig/chronyd && systemctl restart chronyd )
 echo "Chrony service is running with chrony user"
 echo "DONE"
-# ===========================================================================================
+# ==============================================================================================================
 echo ">>> 5.1.8 Ensure at/cron is restricted to authorized users"
 rm -f /etc/cron.deny
 rm -f /etc/at.deny
